46ada259333e400746d54ff68cff26387b1f62da
gongyisheng <yishenggong9437@gmail.com>
2026-02-01 23:13:07 -0800

    add miles

---
+++ miles/lora-megatron/readme.md
@@ -0,0 +1,32 @@
+# miles - lora megatron dev
+
+# env
+ssh radixark02
+```
+# use specific GPUs (e.g., GPU 0,1,2)
+docker create --gpus '"device=0,1,2,3"' --cap-add SYS_PTRACE --security-opt seccomp=unconfined --privileged --shm-size 32G --ulimit nofile=65536:65536 --ulimit memlock=-1 --ulimit stack=67108864 --ipc=host -v /home/yisheng:/workspace -v /data/cache/huggingface:/root/.cache/huggingface -v /data:/data --name sglang-rl-yishenggong radixark/miles:latest sleep infinity
+
+docker start sglang-rl-yishenggong
+docker exec -it sglang-rl-yishenggong bash
+
+# install miles
+rm -r miles
+git clone --branch miles-lora-megatron --single-branch https://github.com/yushengsu-thu/miles.git 
+cd miles
+pip install -e .
+cd ..
+
+# install sglang
+git clone https://github.com/sgl-project/sglang.git
+cd sglang
+pip install -e "python"
+cd ..
+
+# install megatron bridge
+git clone --branch merged-megatron-0.16.0rc0 --single-branch https://github.com/yushengsu-thu/Megatron-Bridge.git
+cd Megatron-Bridge
+pip install -e . --no-deps --no-build-isolation
+pip install megatron-energon --no-deps
+pip install multi-storage-client --no-deps
+cd ..
+```
---
+++ miles/lora-megatron/run_fsdp_lora.py
@@ -0,0 +1,130 @@
+import miles.utils.external_utils.command_utils as U
+import os
+
+MODEL_NAME = "Qwen3-4B"
+ENABLE_LORA = U.get_bool_env_var("ENABLE_LORA", "0")
+ENABLE_LORA = True
+ENABLE_EVAL = True
+
+CUDA_VISIBLE_DEVICES = "4,5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = CUDA_VISIBLE_DEVICES
+
+def prepare():
+    U.exec_command("mkdir -p /root/models /root/datasets")
+    U.exec_command(f"hf download Qwen/{MODEL_NAME} --local-dir /root/models/{MODEL_NAME}")
+    U.hf_download_dataset("zhuzilin/gsm8k")
+    U.hf_download_dataset("zhuzilin/dapo-math-17k")
+    U.hf_download_dataset("zhuzilin/aime-2024")
+
+
+def execute():
+    ckpt_args = f"--hf-checkpoint /root/models/{MODEL_NAME} "
+
+    WANDB_API_KEY = "a37f4796e6205800c4212556a38e1319b5f144b7"
+
+    wandb_args = (
+        "--use-wandb "
+        "--wandb-project miles-lora-test "
+        "--wandb-group qwen3-4B-lora-dapo-lr2e-5 "
+        f"--wandb-key {WANDB_API_KEY} "
+        "--disable-wandb-random-suffix "
+    )
+
+    lora_args = (
+        (
+            "--lora-rank 32 "
+            "--lora-alpha 32 "
+            "--target-modules all-linear "
+            f"--save /root/models/{MODEL_NAME}-lora-ckpt "
+            # "--lora-sync-from-tensor "
+        )
+        if ENABLE_LORA
+        else ""
+    )
+
+    rollout_args = (
+        "--prompt-data /root/datasets/dapo-math-17k/dapo-math-17k.jsonl "
+        "--input-key prompt "
+        "--label-key label "
+        "--apply-chat-template "
+        "--rollout-shuffle "
+        "--rm-type deepscaler "
+        f"--num-rollout {3000 if U.get_env_enable_infinite_run() else 60} "
+        "--rollout-batch-size 16 "
+        "--n-samples-per-prompt 8 "
+        "--rollout-max-response-len 2048 "
+        "--rollout-temperature 1 "
+        "--over-sampling-batch-size 64 "
+        "--dynamic-sampling-filter-path miles.rollout.filter_hub.dynamic_sampling_filters.check_reward_nonzero_std "
+        "--global-batch-size 128 "
+        "--balance-data "
+    )
+
+    eval_args = (
+        f"{'--eval-interval 5 ' if ENABLE_EVAL else ''}"
+        "--eval-prompt-data aime24 /root/datasets/aime-2024/aime-2024.jsonl "
+        "--n-samples-per-eval-prompt 2 "
+        "--eval-max-response-len 16384 "
+        "--eval-top-k 1 "
+    )
+
+    grpo_args = (
+        "--advantage-estimator grpo "
+        # "--use-kl-loss "
+        "--kl-loss-coef 0.00 "
+        "--kl-loss-type low_var_kl "
+        "--kl-coef 0.00 "
+        "--entropy-coef 0.00 "
+        "--eps-clip 0.2 "
+        "--eps-clip-high 0.28 "
+    )
+
+    optimizer_args = (
+        "--optimizer adam "
+        f"--lr {'2e-5' if ENABLE_LORA else '1e-6'} "
+        "--lr-decay-style constant "
+        "--weight-decay 0.1 "
+        "--adam-beta1 0.9 "
+        "--adam-beta2 0.98 "
+    )
+
+    sglang_args = "--rollout-num-gpus-per-engine 2 " "--sglang-decode-log-interval 1000 " "--sglang-enable-metrics "
+
+    fsdp_args = (
+        # Set to true for FULL_STATE_DICT mode, false for SHARDED_STATE_DICT mode (default)
+        # "--fsdp-full-params "  # Uncomment this line to enable full params mode
+        # Set the bucket size for weight update
+        "--update-weight-buffer-size 536870912 "  # 512MB
+    )
+
+    misc_args = (
+        "--actor-num-nodes 1 "
+        "--actor-num-gpus-per-node 4 "
+        "--colocate "
+        "--offload-rollout-level kv_cache weight "
+        "--train-backend fsdp "
+    )
+
+    train_args = (
+        f"{ckpt_args} "
+        f"{lora_args} "
+        f"{rollout_args} "
+        f"{optimizer_args} "
+        f"{grpo_args} "
+        f"{sglang_args} "
+        f"{wandb_args} "
+        f"{eval_args} "
+        f"{fsdp_args} "
+        f"{misc_args} "
+    )
+
+    U.execute_train(
+        train_args=train_args,
+        num_gpus_per_node=4,
+        megatron_model_type=None,
+    )
+
+
+if __name__ == "__main__":
+    prepare()
+    execute()
